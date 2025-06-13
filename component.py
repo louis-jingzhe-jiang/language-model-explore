d91957c00b5bb7ff3769c7b7c29be3b6fbff5df9
louis-jingzhe-jiang <jiang.jingzhe@icloud.com>
2025-06-13 16:58:03 -0400

    updates

---
+++ component.py
@@ -0,0 +1,112 @@
+"""
+Next step: add support for mask; add dropout
+"""
+
+import torch
+from torch import nn
+import math
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x:torch.Tensor):
+        return x
+
+
+class SingleHeadedAttention(nn.Module):
+    """
+    Single-headed attention layer
+
+    Args:
+        d_model (int): The dimensions for word embedding for this model
+        d_k (int): The dimension for query and key vector
+        d_v (int): The dimension for value vector
+    """
+    def __init__(self, d_model:int, d_k:int, d_v:int):
+        super().__init__()
+        # weights for q, k, and v
+        self.d_k = d_k
+        self.wq:torch.nn.Linear = nn.Linear(d_model, d_k)
+        self.wk:torch.nn.Linear = nn.Linear(d_model, d_k)
+        self.wv:torch.nn.Linear = nn.Linear(d_model, d_v)
+
+    def forward(self, x:torch.Tensor):
+        # calculate q, k and v projections
+        q:torch.Tensor = self.wq(x)
+        k:torch.Tensor = self.wk(x)
+        v:torch.Tensor = self.wv(x)
+        # calculate attention score
+        scores:torch.Tensor = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.d_k)
+        # combine attention score with value projection
+        result:torch.Tensor = torch.matmul(torch.softmax(scores, -1), v)
+        return result
+
+
+class MultiHeadedAttention(nn.Module):
+    """
+    Multi-headed attention layer. Output dimension will be `d_v * h`
+
+    Args:
+        d_model (int): The dimensions for word embedding for this model
+        h (int): Number of attention heades
+        d_v (int): The dimension for value vector in each attention head
+    
+    Note:
+        `d_model` must be divisible by `h`
+    """
+    def __init__(self, d_model:int, h:int, d_v:int):
+        super().__init__()
+        # make sure `d_model` is divisible by `h`
+        assert d_model % h == 0
+        self.h:int = h
+        self.d_k:int = d_model // h
+        self.d_v:int = d_v
+        # weights for q, k, and v
+        self.wq:torch.nn.Linear = nn.Linear(d_model, self.d_k * h)
+        self.wk:torch.nn.Linear = nn.Linear(d_model, self.d_k * h)
+        self.wv:torch.nn.Linear = nn.Linear(d_model, self.d_v * h)
+
+    def forward(self, x:torch.Tensor):
+        # get dimensions of input
+        batch_size, d_context, d_model = x.shape
+        # calculate q, k and v projections and reshape to multiple heads
+        q:torch.Tensor = self.wq(x).view(batch_size, d_context, self.h, self.d_k)
+        k:torch.Tensor = self.wk(x).view(batch_size, d_context, self.h, self.d_k)
+        v:torch.Tensor = self.wv(x).view(batch_size, d_context, self.h, self.d_v)
+        # transpose q, k, and v to make attention head to be the second-major axis
+        q = q.transpose(1, 2) # (batch_size, self.h, d_context, self.d_k)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2) # (batch_size, self.h, d_context, self.d_v)
+        # calculate attention scores
+        scores:torch.Tensor = torch.matmul(q, k.transopose(-2, -1)) / math.sqrt(self.d_k)
+        # combine attention score with value projection
+        result:torch.Tensor = torch.matmul(torch.softmax(scores, -1), v)
+        return result
+        
+
+class FeedForward(nn.Module):
+    """
+    Multi-headed attention layer
+
+    Args:
+        d_in (int): The embedding dimensions for input
+        d_ff (int): The dimension for feed-forward network
+        d_out (int): The embedding dimension for output
+    """
+    def __init__(self, d_in:int, d_ff:int, d_out:int):
+        super().__init__()
+        self.layer1:torch.nn.Linear = nn.Linear(d_in, d_ff)
+        self.relu:torch.nn.ReLU = nn.ReLU()
+        self.layer2:torch.nn.Linear = nn.Linear(d_ff, d_out)
+    
+    def forward(self, x:torch.Tensor):
+        result:torch.Tensor = self.layer1(x)
+        result = self.relu(result)
+        result = self.layer2(result)
+        return result
+    
+
+if __name__ == "__main__":
+    pass
--- transformer.py
+++ transformer.py
@@ -1,22 +1,61 @@
+"""
+next steps: add dropout
+"""
+
 import torch
 from torch import nn
-import attention
+import component
 
 
 class DecoderLayer(nn.Module):
     def __init__(self):
-        pass
+        super().__init__()
 
     def forward(self, x):
         pass
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self):
-        pass
+    """
+    Transformer encoder lahyer using multi-headed attention
+
+    Args:
+        d_model (int): The dimensions for word embedding for this model
+        h (int): Number of attention heades
+        d_ff (int): The dimension for feed-forward network
+        d_out (int): The embedding dimension for output
+    """
+    def __init__(self, d_model, h, d_ff):
+        assert d_model % h == 0
+        super().__init__()
+        self.attention = component.MultiHeadedAttention(d_model, h, d_model // h)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.feed_forward = component.FeedForward(d_model, d_ff, d_model)
+        self.norm2 = nn.LayerNorm(d_model)
 
     def forward(self, x):
-        pass
+        att:torch.Tensor = self.attention(x)
+        att = self.norm1(x + att)
+        ff:torch.Tensor = self.feed_forward(att)
+        ff = self.norm2(att + ff)
+        return ff
+
+
+class Encoder(nn.Module):
+    """
+    
+    """
+    def __init__(self, num_embeds:int, d_model:int, n_layer:int, h:int, d_ff:int):
+        super().__init__()
+        self.embedding:torch.nn.Embedding = nn.Embedding(num_embeds, d_model)
+        self.layers:torch.nn.ModuleList = nn.ModuleList([EncoderLayer(d_model, h, d_ff) 
+                                                         for _ in range(n_layer)])
+        self.norm:torch.nn.LayerNorm = nn.LayerNorm(d_model)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return self.norm(x)
 
 
 if __name__ == "__main__":
