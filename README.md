772c0a1ba1fec5b3f772efc11031986b5d4ec9b2
louis-jingzhe-jiang <jiang.jingzhe@icloud.com>
2025-06-13 10:34:00 -0400

    updates

---
+++ README.md
@@ -0,0 +1,2 @@
+# language-model-explore
+
---
+++ transformer.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+import attention
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+
+if __name__ == "__main__":
+    pass
